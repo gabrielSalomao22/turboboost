a213c23ff53d8bf2695cdfbb074d3b6efe61f6a3
Gabriel Salomão <strectrok@gmail.com>
2021-08-17 16:03:41 -0300

    Initial commit

---
+++ src/main/java/com/example/turboboost/TurboboostApplication.java
@@ -0,0 +1,13 @@
+package com.example.turboboost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TurboboostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TurboboostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/turboboost/TurboboostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.turboboost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurboboostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
